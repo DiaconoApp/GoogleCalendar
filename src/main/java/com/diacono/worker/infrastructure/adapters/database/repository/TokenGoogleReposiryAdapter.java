511278cf5683da5d1d653cb60caebb035eea398a
ricardoOliveiraN <ricardo.nicolau@sptech.school>
2026-05-17 21:45:42 -0300

    feat: add log

--- src/main/java/com/diacono/worker/infrastructure/adapters/database/repository/TokenGoogleReposiryAdapter.java
+++ src/main/java/com/diacono/worker/infrastructure/adapters/database/repository/TokenGoogleReposiryAdapter.java
@@ -4,6 +4,8 @@ import com.diacono.worker.application.port.out.TokenGoogleRepository;
 import com.diacono.worker.domain.TokenGoogle;
 import com.diacono.worker.infrastructure.adapters.database.entity.TokenGoogleEntity;
 import com.diacono.worker.infrastructure.mapper.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -20,6 +22,10 @@ public class TokenGoogleReposiryAdapter implements TokenGoogleRepository {
     @Override
     public List<TokenGoogle> findAllTokensRefresh() {
         List<TokenGoogleEntity> tokenGoogleEntities = jpaTokenGoogleRepository.findAll();
+        if(tokenGoogleEntities.isEmpty()) {
+            Logger log = LoggerFactory.getLogger(TokenGoogleReposiryAdapter.class);
+            log.warn("Nenhum token de atualização do Google encontrado no banco de dados.");
+        }
         return mapper.toListTokenGoogle(tokenGoogleEntities);
     }
 }
